d15c18fe435c9694aba394fd590e6e1021a9c07d
MBAIAMNE <fredmbaiamne@gmail.com>
2026-01-09 11:34:02 +0100

    Update urls.py

--- tchad_opportunites/urls.py
+++ tchad_opportunites/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', core_views.home, name='home'),
-    path('emplois/', core_views.job_list, name='job_list'),
-    path('formations/', core_views.formation_list, name='formation_list'),
-    path('stages/', core_views.stage_list, name='stage_list'),
-    path('appels/', core_views.appel_list, name='appel_list'),
+    path('users/', include('django.contrib.auth.urls')),
+    path('', include('announcements.urls')),  # Ton app annonces d’avant
 ]
