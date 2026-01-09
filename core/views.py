b13f1054bcca00f82995e9e965e8879c67b78403
MBAIAMNE <fredmbaiamne@gmail.com>
2026-01-09 11:02:23 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from announcements.models import Annonce
+from announcements.models import Announcement  # Modèle correct : Announcement
+
 def home(request):
     return render(request, 'home.html')
 
 def job_list(request):
-    annonces = Annonce.objects.filter(type='emploi')  # Change 'type' si ton champ s'appelle category
+    annonces = Announcement.objects.all()  # Temporaire : toutes les annonces (on filtre après)
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Emplois & Stages'})
 
 def formation_list(request):
-    annonces = Annonce.objects.filter(type='formation')
+    annonces = Announcement.objects.all()
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Formations & Bourses'})
 
 def stage_list(request):
-    annonces = Annonce.objects.filter(type='stage')
+    annonces = Announcement.objects.all()
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Stages'})
 
 def appel_list(request):
-    annonces = Annonce.objects.filter(type='appel')
+    annonces = Announcement.objects.all()
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Appels d’offres & Concours'})
