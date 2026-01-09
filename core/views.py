18cec1ba572d7099430567ddd219309306aff6cf
MBAIAMNE <fredmbaiamne@gmail.com>
2026-01-09 11:08:38 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
-from announcements.models import Announcement  # Modèle correct : Announcement
+from announcements.models import *  # Import tout pour éviter l'erreur
 
 def home(request):
     return render(request, 'home.html')
 
 def job_list(request):
-    annonces = Announcement.objects.all()  # Temporaire : toutes les annonces (on filtre après)
+    annonces = Annonce.objects.all()  # Affiche tout pour l’instant
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Emplois & Stages'})
 
 def formation_list(request):
-    annonces = Announcement.objects.all()
+    annonces = Annonce.objects.all()
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Formations & Bourses'})
 
 def stage_list(request):
-    annonces = Announcement.objects.all()
+    annonces = Annonce.objects.all()
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Stages'})
 
 def appel_list(request):
-    annonces = Announcement.objects.all()
+    annonces = Annonce.objects.all()
     return render(request, 'list.html', {'annonces': annonces, 'title': 'Appels d’offres & Concours'})
