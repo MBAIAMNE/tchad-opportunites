bb7835b2c189579af072d9d68bcc0cf305caf872
MBAIAMNE <fredmbaiamne@gmail.com>
2026-01-09 10:24:12 +0100

    Update urls.py

--- tchad_opportunites/urls.py
+++ tchad_opportunites/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('', include('core.urls')),
-    path('', include('announcements.urls')),  # <-- Ajoute cette ligne
+    path('', core_views.home, name='home'),
+    path('emplois/', core_views.job_list, name='job_list'),
+    path('formations/', core_views.formation_list, name='formation_list'),
+    path('stages/', core_views.stage_list, name='stage_list'),
+    path('appels/', core_views.appel_list, name='appel_list'),
 ]
