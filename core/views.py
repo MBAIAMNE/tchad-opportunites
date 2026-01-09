05589c194108f2255460334007c2633de813adac
MBAIAMNE <fredmbaiamne@gmail.com>
2026-01-09 11:32:41 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Annonce  # Ou Announcement si c’était en anglais
 
 def home(request):
-    return render(request, 'home.html')
+    annonces = Annonce.objects.all().order_by('-date_creation')  # Toutes les annonces sur l’accueil
+    return render(request, 'home.html', {'annonces': annonces})
 
-def job_list(request):
-    return render(request, 'list.html', {'title': 'Emplois & Stages'})
-
-def formation_list(request):
-    return render(request, 'list.html', {'title': 'Formations & Bourses'})
-
-def stage_list(request):
-    return render(request, 'list.html', {'title': 'Stages'})
-
-def appel_list(request):
-    return render(request, 'list.html', {'title': 'Appels d’offres & Concours'})
+def annonce_detail(request, pk):
+    annonce = get_object_or_404(Annonce, pk=pk)
+    return render(request, 'annonce_detail.html', {'annonce': annonce})
